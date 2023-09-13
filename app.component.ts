af3c7dd5f7d88c9599c4f61c6302fce3d6d69cfd
Manubhati <136340647+Manubhati@users.noreply.github.com>
2023-09-13 11:35:33 +0530

    1 file modified

--- app.component.ts
+++ app.component.ts
@@ -17,7 +17,6 @@ export class AppComponent {
   public data: Object[] = getDefaultData();
   created() {
     this.spreadsheetObj.cellFormat({ fontWeight: 'bold' }, 'E31:F31');
-    this.spreadsheetObj.cellFormat({ textAlign: 'right' }, 'E31');
     this.spreadsheetObj.numberFormat('$#,##0.00', 'F2:F31');
   }
 }
